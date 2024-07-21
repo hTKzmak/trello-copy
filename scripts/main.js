9b5e1f7f71ce87c9ce5339eb6bf4031ddee121f7
hTKzmak <ineedrtx@gmail.com>
2024-07-21 15:52:29 +0300

    adding TinyMCE library to textarea

--- scripts/main.js
+++ scripts/main.js
@@ -176,6 +176,7 @@ function addWindowModal(cardItem, columnItemData) {
     windowMainInfo.appendChild(cardDescriptionTextarea);
     windowMainInfo.appendChild(options)
 
+
     // Добавление элементов на страницу
     windowElement.appendChild(header);
     windowElement.appendChild(windowMainInfo);
@@ -185,6 +186,23 @@ function addWindowModal(cardItem, columnItemData) {
     // Добавление модального окна на страницу
     document.body.appendChild(modalWindowContainer);
 
+
+    // подключение tinymce для #card_description после создания модального окна
+    tinymce.init({
+        selector: '#card_description',
+        license_key: 'gpl',
+        statusbar: false,
+        promotion: false,
+        menubar: true,
+        language: 'ru',
+        plugins: 'lists emoticons',
+        // forced_root_block: 'div',
+        // newline_behavior: 'linebreak',
+        toolbar: 'fontfamily fontsize | bold italic underline | alignleft aligncenter alignright alignjustify | emoticons | backcolor forecolor removeformat | bullist numlist outdent indent | undo redo',
+        mobile: {
+            toolbar_mode: 'floating'
+        }
+    })
 }
 
 // функция по созданию колонки 
