b168b33cbbc911a4134036cd30bba571af1bded7
hTKzmak <ineedrtx@gmail.com>
2024-08-29 21:00:24 +0300

    updated color

--- scripts/newMain.js
+++ scripts/newMain.js
@@ -193,34 +193,75 @@ function updateCardColor(card, color) {
 
 // Функция для создания кнопки изменения цвета
 function createColorButton(container, card, columnItemData) {
-    const colorPicker = document.createElement('input');
-    colorPicker.type = 'text';
-    colorPicker.className = 'buttonStyle';
-    colorPicker.value = 'Изменить цвет';
-    colorPicker.setAttribute('data-coloris', '');
 
-    colorPicker.addEventListener('click', () => {
+    const button = document.createElement('button');
+    button.className = 'buttonStyle';
+    button.innerHTML = 'Изменить цвет';
+
+    const colorPicker = document.createElement('input');
+    colorPicker.type = 'color';
+    colorPicker.className = 'buttonStyle';
+    colorPicker.setAttribute('data-coloris', '');
+    colorPicker.style = `
+        position: absolute;
+        width: 100%;
+        left: 0;
+        top: 0;
+        opacity: 0;
+    `
+
+    colorPicker.addEventListener('input', (e) => {
+        console.log(e.target.value)
+        updateCardColor(document.getElementById(card.id), e.target.value);
+        const index = columnItemData.cards.findIndex(elem => elem.id == card.id);
+
+        if (index !== -1) {
+            columnItemData.cards[index].color = e.target.value;
+        }
     })
 
-    Coloris({
-        el: '#coloris',
-        parent: container,
-        theme: 'default',
-        themeMode: 'light',
-        onChange: (color) => {
-            colorPicker.value = 'Изменить цвет';
-            updateCardColor(document.getElementById(card.id), color);
-            const index = columnItemData.cards.findIndex(elem => elem.id == card.id);
-
-            if (index !== -1) {
-                columnItemData.cards[index].color = color;
-            }
-        }
+    button.addEventListener('click', () => {
+        colorPicker.click();
     });
 
 
-    container.appendChild(colorPicker);
+    button.appendChild(colorPicker)
+
+    container.appendChild(button);
 }
+
+// // Функция для создания кнопки изменения цвета
+// function OLD_FUNCTION_createColorButton(container, card, columnItemData) {
+//     const colorPicker = document.createElement('input');
+//     colorPicker.type = 'text';
+//     colorPicker.className = 'buttonStyle';
+//     colorPicker.value = 'Изменить цвет';
+//     colorPicker.setAttribute('data-coloris', '');
+
+//     colorPicker.addEventListener('click', () => {
+//     })
+
+//     Coloris({
+//         el: '#coloris',
+//         parent: container,
+//         theme: 'default',
+//         themeMode: 'light',
+//         onChange: (color) => {
+//             colorPicker.value = 'Изменить цвет';
+//             updateCardColor(document.getElementById(card.id), color);
+//             const index = columnItemData.cards.findIndex(elem => elem.id == card.id);
+
+//             if (index !== -1) {
+//                 columnItemData.cards[index].color = color;
+//             }
+//         }
+//     });
+
+
+//     container.appendChild(colorPicker);
+// }
+
+
 
 // Функция для создания меню с опциями
 function createMenuWindow(button, item, container, type, input, columnItemData) {
